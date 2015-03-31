99282f3059114bd332763795c15f8d00a9ba45e4
Greg Otte <greg.otte@samaritanministries.org>
2015-03-31 14:00:35 -0500

    Added custom redirect filter to fix security problems on qa and production

---
+++ source/WebHost/CustomSessionAuthenticationModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IdentityModel.Services;
+using System.Net;
+using System.Web;
+
+namespace Thinktecture.AuthorizationServer.WebHost
+{
+    public class CustomSessionAuthenticationModule : SessionAuthenticationModule
+    {
+        protected override void OnAuthenticateRequest(object sender, EventArgs args)
+        {
+            base.OnAuthenticateRequest(sender, args);
+            ConvertToHttpsIfNeeded(sender);
+        }
+
+        private static void ConvertToHttpsIfNeeded(object sender)
+        {
+            var response = ((HttpApplication) sender).Response;
+            var request = ((HttpApplication) sender).Request;
+
+            if (response.StatusCode == (int) HttpStatusCode.Redirect && request.Url.Scheme.ToLower() == "https" &&
+                response.RedirectLocation.ToLower().StartsWith("http:"))
+            {
+                response.RedirectLocation = response.RedirectLocation.Remove(0, 4).Insert(0, "https");
+            }
+        }
+    }
+}
